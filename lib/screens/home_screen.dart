081cc5d5696b4c0d056a599b6daf8f3309fb3437
Nayalash Mohammad <1nayalashm@gmail.com>
2019-09-14 15:12:36 -0400

    Initial Commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("SpeakNow App"),
+        backgroundColor: Colors.redAccent,
+      ),
+        backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height: 50.0),
+            SizedBox(
+                height: 250.0,
+                child: Carousel(
+                  images: [
+                    NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+                    NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+                    ExactAssetImage("assets/images/LaunchImage.jpg")
+                  ],
+                ),
+
+            ),
+          ],
+        ),
+      ),
+      );
+  }
+}
