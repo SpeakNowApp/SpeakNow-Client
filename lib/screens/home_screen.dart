05ef1595a738f5a125188d2e7a50ef7ce9e3bc8f
Hussein <hznagri@uwaterloo.ca>
2019-09-14 15:53:47 -0400

    added text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,30 +14,31 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text("SpeakNow App"),
         backgroundColor: Colors.redAccent,
       ),
-        backgroundColor: Colors.white,
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            SizedBox(height: 50.0),
-            SizedBox(
-                height: 250.0,
-                child: Carousel(
-                  images: [
-                    NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
-                    NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
-                    NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),                  ],
-                ),
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 50.0),
+          SizedBox(
+            height: 250.0,
+            child: Carousel(
+              images: [
+                NetworkImage(
+                    'https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+                NetworkImage(
+                    'https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+                NetworkImage(
+                    'https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+              ],
             ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Text(
-                "Translation: HI"
-              ),
-            )
-          ],
-        )
-      ),
-      );
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text("Translation: HI"),
+          )
+        ],
+      )),
+    );
   }
 }
