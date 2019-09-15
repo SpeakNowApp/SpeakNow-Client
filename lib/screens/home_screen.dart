570db8c62b5d1f43d1aed22273c1d7811814ad74
Hussein <hznagri@uwaterloo.ca>
2019-09-14 20:22:03 -0400

    basic

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -7,9 +8,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  var _languages = ['English', 'French', 'Spanish'];
   var _currentItemSelected = 'English';
+  var _languages = {"English": "en", "French": "fr", "Spanish": "es"};
+  var text = "My name is Hussein";
 
   @override
   Widget build(BuildContext context) {
@@ -21,57 +22,59 @@ class _HomeScreenState extends State<HomeScreen> {
       backgroundColor: Colors.white,
       body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 50.0),
-              SizedBox(
-                height: 250.0,
-                child: Carousel(
-                  images: [
-                    NetworkImage('https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
-                    NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
-                    NetworkImage('https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),                  ],
-                ),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 50.0),
+          SizedBox(
+            height: 250.0,
+            child: Carousel(
+              images: [
+                NetworkImage(
+                    'https://cdn-images-1.medium.com/max/2000/1*GqdzzfB_BHorv7V2NV7Jgg.jpeg'),
+                NetworkImage(
+                    'https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+                NetworkImage(
+                    'https://cdn-images-1.medium.com/max/2000/1*wnIEgP1gNMrK5gZU7QS0-A.jpeg'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              "Translation: INSERT HERE",
+              style: TextStyle(
+                fontSize: 30,
               ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  "Translation: INSERT HERE",
-                  style: TextStyle(
-                    fontSize: 30,
-                  ),
-                ),
-              ),
-
-              Center(
-                child: DropdownButton<String>(
-
-                  items: _languages.map((String dropDownStringItem) {
-                    return DropdownMenuItem<String>(
-                      value: dropDownStringItem,
-                      child: Text(dropDownStringItem),
-                    );
-                  }).toList(),
-
-                  onChanged: (String newValueSelected) {
-                    // Your code to execute, when a menu item is selected from drop down
-                    _onDropDownItemSelected(newValueSelected);
-                  },
-
-                  value: _currentItemSelected,
-
-                ),
-              ),
-            ],
-          )
-      ),
+            ),
+          ),
+          Center(
+            child: DropdownButton<String>(
+              items: _languages.keys.map((String dropDownStringItem) {
+                return DropdownMenuItem<String>(
+                  value: dropDownStringItem,
+                  child: Text(dropDownStringItem),
+                );
+              }).toList(),
+              onChanged: (String newValueSelected) {
+                // Your code to execute, when a menu item is selected from drop down
+                _onDropDownItemSelected(newValueSelected);
+              },
+              value: _currentItemSelected,
+            ),
+          ),
+          Text(text)
+        ],
+      )),
     );
   }
 
-  void _onDropDownItemSelected(String newValueSelected) {
+  void _onDropDownItemSelected(String newValueSelected) async {
+    var request = await http.get(
+        "https://translate.google.com/?client=safari&rls=en&oe=UTF-8&um=1&ie=UTF-8&hl=en&client=tw-ob#en/${_languages[_currentItemSelected]}/${text}");
+    print(request.body);
+
     setState(() {
       this._currentItemSelected = newValueSelected;
     });
   }
-
 }
